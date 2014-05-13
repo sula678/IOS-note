944f079de73cacaaac854ba81dfa4862f7bfbabf
sula678 <sula678@gmail.com>
2014-05-13 23:43:44 +0900

    add short url in shorty

--- Shorty/Shorty/ShortyViewController.h
+++ Shorty/Shorty/ShortyViewController.h
@@ -8,8 +8,13 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ShortyViewController : UIViewController
+@interface ShortyViewController : UIViewController <UIWebViewDelegate, NSURLConnectionDelegate, NSURLConnectionDataDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *urlField;
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
 - (IBAction)loadLocation:(id)sender;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *shortenButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *shortLabel;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *clipboardButton;
+- (IBAction)shortenURL:(id)sender;
+- (IBAction)clipboardURL:(id)sender;
 @end
--- Shorty/Shorty/ShortyViewController.m
+++ Shorty/Shorty/ShortyViewController.m
@@ -9,9 +9,12 @@
 #import "ShortyViewController.h"
 
 @interface ShortyViewController ()
-
+{
+    NSURLConnection *shortenURLConnection;
+    NSMutableData *shortURLData;
+}
 @end
-
+#define kGoDaddyAccountKey @"dd0b771588e04b3f8eec4f26cb6fd10e"
 @implementation ShortyViewController
 
 - (void)viewDidLoad
@@ -35,5 +38,61 @@
     }
     NSURL *url = [NSURL URLWithString:urlText];
     [self.webView loadRequest:[NSURLRequest requestWithURL:url]];
+}
+
+- (void)webViewDidStartLoad:(UIWebView *)webView
+{
+    self.shortenButton.enabled = NO;
+}
+
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView
+{
+    self.shortenButton.enabled = YES;
+    self.urlField.text = webView.request.URL.absoluteString;
+    NSString *urlToShorten = self.webView.request.URL.absoluteString;
+}
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
+{
+    NSString *message = [NSString stringWithFormat: @"A problem occurred trying to load this page: %@", error.localizedDescription];
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Could not load URL" message:message delegate:nil cancelButtonTitle:@"That's Sad" otherButtonTitles:nil];
+    [alert show];
+}
+
+- (IBAction)shortenURL:(id)sender
+{
+    NSString *urlToShorten = self.webView.request.URL.absoluteString;
+    NSString *urlString = [NSString stringWithFormat:@"http://api.x.co/Squeeze.svc/text/%@?url=%@", kGoDaddyAccountKey,[urlToShorten stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+    shortURLData = [NSMutableData new];
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:urlString]];
+    shortenURLConnection = [NSURLConnection connectionWithRequest:request delegate:self];
+    self.shortenButton.enabled = NO;
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
+    self.shortLabel.title = @"failed";
+    self.clipboardButton.enabled = NO;
+    self.shortenButton.enabled = YES;
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+    [shortURLData appendData:data];
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    NSString *shortURLString = [[NSString alloc] initWithData:shortURLData encoding:NSUTF8StringEncoding];
+    self.shortLabel.title = shortURLString;
+    self.clipboardButton.enabled = YES;
+}
+
+- (IBAction)clipboardURL:(id)sender
+{
+    NSString *shortURLString = self.shortLabel.title;
+    NSURL *shortURL = [NSURL URLWithString:shortURLString];
+    [[UIPasteboard generalPasteboard] setURL:shortURL];
 }
 @end
